8efa4f2ba6ca9b848c31bb77d59231dde48a3070
Eduardo Stuart <contato@eduardostuart.com.br>
2016-02-08 11:53:24 -0200

    follow example timeout - typo fix
    Signed-off-by: Eduardo Stuart <contato@eduardostuart.com.br>

--- examples/example.js
+++ examples/example.js
@@ -1,10 +1,10 @@
 Vue.config.debug = true;
 
-// Default timeout 2
+// Default timeout 1
 Vue.use(VueImageLoader,{
     loadInfo: 'Loading',
     loadError:'Ops..something went wrong',
-    timeout:2
+    timeout:1
 });
 
 new Vue().$mount('body');
